53d7c6bfca89e2a9d07d75a688a8a97c2ef23f2c
Lungu Victor <lungulll98@gmail.com>
2025-11-09 13:02:00 +0200

    Add Ex 6

---
+++ src/Ex6.java
@@ -0,0 +1,11 @@
+public class Ex6 {
+    public static void main(String[] args) {
+
+            int n = 100;
+            for (int i = 2; i <= n; i=i+2) {
+            System.out.println("Numerele pare sunt: " + i);
+
+        }
+
+    }
+}
