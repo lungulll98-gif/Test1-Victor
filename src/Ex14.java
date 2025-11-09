7ac7c304273465c53cb123f01509f49148d54c47
Lungu Victor <lungulll98@gmail.com>
2025-11-09 14:07:57 +0200

    Add Ex 14

---
+++ src/Ex14.java
@@ -0,0 +1,19 @@
+public class Ex14 {
+    public static void main(String[] args) {
+
+                for (int i = 0; i < 21; i++) {
+                if (i == 14) {
+                System.out.println("Aici trebuia sa fie 14");
+                break;
+
+                }
+                System.out.println(i);
+            }
+
+                for (int i = 15; i < 21; i++) {
+                 System.out.println(i);
+                }
+
+        }
+    }
+
