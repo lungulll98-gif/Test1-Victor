262ccc3482754b1f2765cab238c4e678c69f757c
Lungu Victor <lungulll98@gmail.com>
2025-11-09 12:25:16 +0200

    Add Ex 1

---
+++ src/Ex1.java
@@ -0,0 +1,10 @@
+public class Ex1 {
+    public static void main(String[] args) {
+
+        System.out.println("Rezultatele finale: ");
+        System.out.println(-6+8*6);
+        System.out.println((44+8)%5);
+        System.out.println(5+20/3*2-8%4);
+
+    }
+}
