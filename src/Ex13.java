c3f687646f31d63679fa31988cf5f486ff65d5b7
Lungu Victor <lungulll98@gmail.com>
2025-11-09 13:58:13 +0200

    Add Ex 13

---
+++ src/Ex13.java
@@ -0,0 +1,14 @@
+public class Ex13 {
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 20; i++) {
+            if(i == 13) {
+                System.out.println(i);
+                break;
+            }
+            System.out.println(i);
+
+        }
+
+    }
+}
