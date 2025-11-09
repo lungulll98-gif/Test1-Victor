82ecedf540777a39f8f2fcc5204456788d9b1554
Lungu Victor <lungulll98@gmail.com>
2025-11-09 14:21:37 +0200

    Add Ex 16

---
+++ src/Ex16.java
@@ -0,0 +1,14 @@
+public class Ex16 {
+    public static void main(String[] args) {
+
+
+        int[] numere = {5, 10, 15, 20, 25};
+        int lungime = numere.length;
+        int primulIndice = 0;
+        int ultimulIndice = lungime - 1;
+
+        System.out.println("Lungimea array-ului: "        + lungime);
+        System.out.println("Indicele primului element: "  + primulIndice);
+        System.out.println("Indicele ultimului element: " + ultimulIndice);
+    }
+}
