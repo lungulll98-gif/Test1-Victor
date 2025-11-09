e3adb20d1043bef1a461e0598848d670e4c29138
Lungu Victor <lungulll98@gmail.com>
2025-11-09 12:56:31 +0200

    Add Ex 5

---
+++ src/Ex5.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Ex5 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Introduceti parola: ");
+        String parola = scanner.next();
+        System.out.print("Parola introdusa este: " + parola);
+
+
+
+
+    }
+}
